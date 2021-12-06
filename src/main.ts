c319010a2542cd6fbabfd018baaaa1c790426c2d
ext.liangkaiwen1 <ext.liangkaiwen1@jd.com>
2021-12-06 15:08:03 +0800

    feat: 安装ui组件.

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import Vant from 'vant'
+
+createApp(App).use(store).use(router).use(ElementPlus).use(Vant).mount('#app')
