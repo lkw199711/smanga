f5014494c529d5f924fe56d611b6d0928705df6e
ext.liangkaiwen1 <ext.liangkaiwen1@jd.com>
2021-12-16 19:32:23 +0800

    test: nav分离.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
     <div class="main">
-        <el-menu
-                :default-active="activeIndex2"
-                class="el-menu-demo"
-                mode="horizontal"
-                background-color="#545c64"
-                text-color="#fff"
-                active-text-color="#ffd04b"
-                @select="handleSelect"
-        >
-            <el-menu-item index="1">Processing Center</el-menu-item>
-            <el-sub-menu index="2">
-                <template #title>Workspace</template>
-                <el-menu-item index="2-1">item one</el-menu-item>
-                <el-menu-item index="2-2">item two</el-menu-item>
-                <el-menu-item index="2-3">item three</el-menu-item>
-                <el-sub-menu index="2-4">
-                    <template #title>item four</template>
-                    <el-menu-item index="2-4-1">item one</el-menu-item>
-                    <el-menu-item index="2-4-2">item two</el-menu-item>
-                    <el-menu-item index="2-4-3">item three</el-menu-item>
-                </el-sub-menu>
-            </el-sub-menu>
-            <el-menu-item index="3" disabled>Info</el-menu-item>
-            <el-menu-item index="4">Orders</el-menu-item>
-        </el-menu>
-
+        <top-nav></top-nav>
         <div class="view">
             <router-view/>
         </div>
@@ -36,20 +11,11 @@
 
 <script lang="ts">
     import {defineComponent, ref} from 'vue'
+    import topNav from './components/top-nav.vue';
 
     export default defineComponent({
-        setup() {
-            const activeIndex = ref('1')
-            const activeIndex2 = ref('1')
-            const handleSelect = (key: any, keyPath: any) => {
-                console.log(key, keyPath)
-            }
-            return {
-                activeIndex,
-                activeIndex2,
-                handleSelect,
-            }
-        },
+        // 组件
+        components: {topNav},
     })
 </script>
 
---
+++ src/components/top-nav.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="top-nav">
+        <el-menu
+                :default-active="activeIndex2"
+                class="el-menu-demo"
+                mode="horizontal"
+                router
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+                @select="handleSelect"
+        >
+            <el-menu-item index="/">首页</el-menu-item>
+            <el-menu-item index="/setting-path">设置</el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script lang='ts'>
+    import {defineComponent, ref} from 'vue'
+
+    export default defineComponent({
+        setup() {
+            const activeIndex = ref('1')
+            const activeIndex2 = ref('1')
+            const handleSelect = (key: any, keyPath: any) => {
+                // console.log(key, keyPath)
+            }
+            return {
+                activeIndex,
+                activeIndex2,
+                handleSelect,
+            }
+        },
+
+        name: 'top-nav',
+        // 数据
+        data() {
+            return {}
+        },
+
+        // 传值
+        props: [],
+
+        // 引用
+        computed: {},
+
+        // 组件
+        components: {},
+
+        // 方法
+        methods: {},
+
+        // 生命周期
+        created() {
+        },
+    })
+</script>
+
+<style scoped lang='less' type='text/less'>
+
+</style>
