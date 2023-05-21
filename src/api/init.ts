020415d28979d0c68a7b2ce5f780a63974413bc2
lkw199711 <lkw199711@163.com>
2023-05-21 13:35:25 +0800

    fix: 初始化延时变为3分钟

--- src/api/init.ts
+++ src/api/init.ts
@@ -2,13 +2,14 @@
  * @Author: lkw199711 lkw199711@163.com
  * @Date: 2023-05-03 11:35:53
  * @LastEditors: lkw199711 lkw199711@163.com
- * @LastEditTime: 2023-05-21 12:12:50
+ * @LastEditTime: 2023-05-21 13:34:00
  * @FilePath: \smanga\src\api\init.ts
  */
 import {ajax} from './index';
 
 export function system_init(userName: string, passWord: string) {
 	return ajax({
+		timeout: 3 * 60 * 1000,
 		url: 'deploy/database-init',
 		data: {userName, passWord},
 	});
