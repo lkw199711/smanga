5fac4cb0181fbfd224a540d47037c32c9943102d
lkw199711 <lkw199711@163.com>
2023-05-21 17:35:19 +0800

    fix: 队列表生成放在3.3.0

--- php/laravel/app/Http/Controllers/Deploy.php
+++ php/laravel/app/Http/Controllers/Deploy.php
@@ -258,35 +258,6 @@ class Deploy extends Controller
                     UNIQUE INDEX `version`(`version`) USING BTREE);
             ");
 
-            // 生成任务队列表
-            $link->query("
-                CREATE TABLE IF NOT EXISTS `failed_jobs` (
-                    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                    `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `failed_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                    PRIMARY KEY (`id`) USING BTREE,
-                    UNIQUE INDEX `failed_jobs_uuid_unique`(`uuid`) USING BTREE
-                ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-            ");
-
-            $link->query("
-                CREATE TABLE IF NOT EXISTS `jobs` (
-                    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                    `queue` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                    `attempts` tinyint(3) UNSIGNED NOT NULL,
-                    `reserved_at` int(10) UNSIGNED NULL DEFAULT NULL,
-                    `available_at` int(10) UNSIGNED NOT NULL,
-                    `created_at` int(10) UNSIGNED NOT NULL,
-                    PRIMARY KEY (`id`) USING BTREE,
-                    INDEX `jobs_queue_index`(`queue`) USING BTREE
-                    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-            ");
-
             // 插入smanga的用户名密码
             $link->query("INSERT INTO `user` VALUES (1, 'smanga', 'f7f1fe7186209906a97756ff912bb644', NULL, NULL, NULL);");
         }
@@ -515,6 +486,35 @@ class Deploy extends Controller
         }
         // 330
         if (array_search('3.3.0', $vers) === false) {
+            // 生成任务队列表
+            $link->query("
+                CREATE TABLE IF NOT EXISTS `failed_jobs` (
+                    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `failed_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    PRIMARY KEY (`id`) USING BTREE,
+                    UNIQUE INDEX `failed_jobs_uuid_unique`(`uuid`) USING BTREE
+                ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+            ");
+
+            $link->query("
+                CREATE TABLE IF NOT EXISTS `jobs` (
+                    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `queue` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `attempts` tinyint(3) UNSIGNED NOT NULL,
+                    `reserved_at` int(10) UNSIGNED NULL DEFAULT NULL,
+                    `available_at` int(10) UNSIGNED NOT NULL,
+                    `created_at` int(10) UNSIGNED NOT NULL,
+                    PRIMARY KEY (`id`) USING BTREE,
+                    INDEX `jobs_queue_index`(`queue`) USING BTREE
+                    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+            ");
+            
             VersionSql::add([
                 'version' => '3.3.0',
                 'versionDescribe' => '使用laravel重构后端;裁剪模式支持阅读朝向设置;按名称排序按照数字排序方式;新增按id排序.',
