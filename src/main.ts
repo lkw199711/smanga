2a39b8b8c6b98efe398c71c9256ea65b520f3916
lkw199711 <lkw199711@163.com>
2025-02-11 01:50:36 +0800

    style: 角色列表滚动修复.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-08-16 15:09:40
- * @LastEditors: 梁楷文 lkw199711@163.com
- * @LastEditTime: 2024-05-29 11:45:14
+ * @LastEditors: lkw199711 lkw199711@163.com
+ * @LastEditTime: 2025-02-11 01:49:24
  * @FilePath: /smanga/src/main.ts
  */
 import {createApp} from 'vue';
@@ -24,14 +24,9 @@ import './assets/colour/iconfont.js';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import 'vant/lib/index.css';
 
-import {PerfectScrollbar, PerfectScrollbarPlugin} from 'vue3-perfect-scrollbar';
-// import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css';
 import { createPinia } from 'pinia';
 
 import '@/style/public.less';
-// import {Plugin} from 'vue';
-
-// const plugin: Plugin<[]> = PerfectScrollbar as Plugin<[]>;
 
 const appVue = createApp(App);
 
@@ -42,9 +37,6 @@ appVue
 	.use(List)
 	.use(PullRefresh)
 	.use(createPinia());
-	
-// 在这里设置全局配置
-appVue.use(PerfectScrollbarPlugin);
 
 appVue.mount('#app');
 
