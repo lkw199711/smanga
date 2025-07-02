a8529e4edba8dd3c2d5a3de9cc8b74e45a06379f
lkw199711 <lkw199711@163.com>
2025-07-02 15:12:35 +0800

    fix: 编辑元数据按钮修复

--- src/views/manga-info/index.vue
+++ src/views/manga-info/index.vue
@@ -64,7 +64,7 @@
             <el-button class="btn" type="success" @click="collect_manga" v-else>收藏漫画</el-button>
 
             <el-button class="btn" type="primary" @click="open_tag_box">编辑标签</el-button>
-            <el-button class="btn" type="primary" @click="open_covers_edit" v-if="hasManyCover">编辑封面</el-button>
+            <el-button class="btn" type="primary" @click="open_covers_edit">编辑封面</el-button>
             <el-button class="btn" type="primary" @click="open_metas_edit" v-if="hasManyCover">编辑元数据</el-button>
         </div>
 
