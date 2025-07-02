3babf57b04ec3bc098de57af397d1b168eacdfab
lkw199711 <lkw199711@163.com>
2025-07-02 15:17:31 +0800

    fix: 编辑元数据按钮

--- src/views/manga-info/index.vue
+++ src/views/manga-info/index.vue
@@ -64,8 +64,8 @@
             <el-button class="btn" type="success" @click="collect_manga" v-else>收藏漫画</el-button>
 
             <el-button class="btn" type="primary" @click="open_tag_box">编辑标签</el-button>
-            <el-button class="btn" type="primary" @click="open_covers_edit">编辑封面</el-button>
-            <el-button class="btn" type="primary" @click="open_metas_edit" v-if="hasManyCover">编辑元数据</el-button>
+            <el-button class="btn" type="primary" @click="open_covers_edit" v-if="hasManyCover">编辑封面</el-button>
+            <el-button class="btn" type="primary" @click="open_metas_edit">编辑元数据</el-button>
         </div>
 
         <el-dialog :title="$t('rightSidebar.editTags')" v-model="editTagsDialog">
