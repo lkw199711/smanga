4e6a4449df5a076684b1b0d5281a63d75203b434
lkw199711 <lkw199711@163.com>
2025-07-04 12:42:26 +0800

    style: 继续阅读按钮 自动换行.

--- src/views/manga-info/index.vue
+++ src/views/manga-info/index.vue
@@ -57,7 +57,7 @@
         <div class="btn-box bottom">
             <el-button class="btn" type="primary" @click="go_chapter_list">章节列表</el-button>
 
-            <el-button class="btn" type="warning" @click="go_chapter" v-if="latestChapterInfo">
+            <el-button class="btn continue-read" type="warning" @click="go_chapter" v-if="latestChapterInfo">
                 {{ latestChapterInfo.chapter.chapterName }}
                 第{{ latestChapterInfo.page }}页
             </el-button>
@@ -462,6 +462,10 @@ function update_tags(tagsParams: tagItemType[]) {
         z-index: 0;
         filter: blur(2px);
     }
+}
+// 自动换行
+.continue-read{
+    white-space: normal;
 }
 
 .cover-setting {
