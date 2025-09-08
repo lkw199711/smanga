35de75d71db7b3ee4759caed6b67a5bf9c33ca7c
lkw199711 <lkw199711@163.com>
2025-09-08 20:44:38 +0800

    feat: history router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Layout from '@/layout/index.vue';
 import browse from '@/layout/browse.vue';
 
@@ -432,7 +432,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes,
 });
 
