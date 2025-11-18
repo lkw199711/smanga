59f79bab46c095c12a9fb1fb7826cadd17c5525b
lkw199711 <lkw199711@163.com>
2025-11-19 01:42:16 +0800

    fix: 修复引入.

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,8 @@ import './interface';
 import i18n from '@/i18n';
 
 // 按需引入Element Plus组件和样式
-import { ElButton, ElInput, ElDialog, ElForm, ElFormItem, ElSelect, ElOption, ElPagination, ElTable, ElTableColumn, ElConfigProvider, ElMenu, ElMenuItem, ElMenuItemGroup,
-	ElSubMenu
- } from 'element-plus';
-import 'element-plus/es/components/button/style/css';
-import 'element-plus/es/components/input/style/css';
-import 'element-plus/es/components/dialog/style/css';
-import 'element-plus/es/components/form/style/css';
-import 'element-plus/es/components/menu/style/css';
-import 'element-plus/es/components/form-item/style/css';
-import 'element-plus/es/components/select/style/css';
-import 'element-plus/es/components/option/style/css';
-import 'element-plus/es/components/pagination/style/css';
-import 'element-plus/es/components/table/style/css';
-import 'element-plus/es/components/table-column/style/css';
-import 'element-plus/es/components/message/style/css';
-import 'element-plus/es/components/message-box/style/css';
-import 'element-plus/es/components/config-provider/style/css';
-import 'element-plus/es/components/menu-item/style/css';
-import 'element-plus/es/components/menu-item-group/style/css';
-import 'element-plus/es/components/sub-menu/style/css';
+import ElementPlus from 'element-plus';
+import 'element-plus/theme-chalk/index.css';
 
 import 'reset-css';
 import './style/public.less';
@@ -41,26 +23,10 @@ initTheme();
 
 const appVue = createApp(App);
 
-// 注册Element Plus组件
-appVue.component('ElButton', ElButton);
-appVue.component('ElInput', ElInput);
-appVue.component('ElDialog', ElDialog);
-appVue.component('ElForm', ElForm);
-appVue.component('ElFormItem', ElFormItem);
-appVue.component('ElSelect', ElSelect);
-appVue.component('ElOption', ElOption);
-appVue.component('ElPagination', ElPagination);
-appVue.component('ElTable', ElTable);
-appVue.component('ElTableColumn', ElTableColumn);
-appVue.component('ElConfigProvider', ElConfigProvider);
-appVue.component('ElMenu', ElMenu);
-appVue.component('ElMenuItem', ElMenuItem);
-appVue.component('ElMenuItemGroup', ElMenuItemGroup);
-appVue.component('ElSubMenu', ElSubMenu);
-
 appVue
 	.use(router)
 	.use(i18n)
+	.use(ElementPlus)
 	.use(createPinia());
 
 appVue.mount('#app');
