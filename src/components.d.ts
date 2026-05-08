d90db05e2cdb61da4580c7e3608b01ef11492295
lkw199711 <lkw199711@163.com>
2026-05-08 12:39:26 +0800

    feat(components): 添加 ElTree 组件全局类型声明
    - 在全局组件声明中新增 ElTree 类型支持
    - 确保 TypeScript 能正确识别 ElTree 组件
    - 维护组件类型定义的完整性和一致性

--- src/components.d.ts
+++ src/components.d.ts
@@ -60,6 +60,7 @@ declare module 'vue' {
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
     ElTag: typeof import('element-plus/es')['ElTag']
+    ElTree: typeof import('element-plus/es')['ElTree']
     Line: typeof import('./components/charts/line.vue')['default']
     List: typeof import('./components/charts/list.vue')['default']
     ListSkeleton: typeof import('./components/list-skeleton.vue')['default']
